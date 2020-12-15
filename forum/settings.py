649f8a0419d27a7411143ec7f2d3007a3f1e2fc6
Deepanvi <cs18b009@iittp.ac.in>
2020-12-15 21:48:45 +0530

    stat

--- forum/settings.py
+++ forum/settings.py
@@ -130,8 +130,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
